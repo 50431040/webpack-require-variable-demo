355242ac1ff38536054dc0da3f7911b2bb85ee24
jlq-mac <50431040@qq.com>
2022-05-17 00:18:52 +0800

    feat: first commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+
+module.exports = {
+    mode: "production",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]"
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
+}
