2f67731f92b48ce2726e8813ac97d062eafd0815
jlq-mac <50431040@qq.com>
2022-05-17 23:57:19 +0800

    feat: 非图片格式的文件测试

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-function createImg() {
-    const img = document.createElement("img")
-    img.src = require("./images/webpack.png").default
-    document.body.appendChild(img)
-}
+// function createImg() {
+//     const img = document.createElement("img")
+//     img.src = require("./images/webpack.png").default
+//     document.body.appendChild(img)
+// }
 
 // 只会打包webpack文件
-createImg()
+// createImg()
 
 
 // 使用变量的形式创建多个Img
-// function createManyImg() {
-//     const names = ["babel.png", "flutter.png"]
+function createManyImg() {
+    const names = ["babel.png", "flutter.png"]
 
-//     names.forEach((name) => {
-//         const img = document.createElement("img")
-//         img.src = require(`./images2/${name}`).default
-//         img.width = "100"
-//         document.body.appendChild(img)
-//     })
-// }
+    names.forEach((name) => {
+        const img = document.createElement("img")
+        img.src = require(`./images2/${name}`).default
+        img.width = "100"
+        document.body.appendChild(img)
+    })
+}
 
 // 会把images2目录下的所有图片打包
-// createManyImg()
+createManyImg()
